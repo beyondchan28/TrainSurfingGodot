e45a9181f5fc97603fa348d2bb6845afd47ea442
Chandra Gunawan <beyonddbirthday28@gmail.com>
2022-07-31 23:03:13 +0700

    Revert "Human Workers Logic and Robot Workers Logic Done"
    This reverts commit 1cb6f8a1def877e8cdc6e14cbfeea6bdc5ed2ff0.

--- PathFollow.gd
+++ PathFollow.gd
@@ -1,9 +1,7 @@
 extends PathFollow
 
-onready var look = $MeshInstance/LookMe
 
 var speed = 20
 
 func _process(delta):
 	set_offset(get_offset() + speed * delta)
-
--- Worker.gd
+++ Worker.gd
@@ -9,8 +9,6 @@ var curr_state
 var distance
 var target
 
-const GRAVITY = 98
-
 export(Array, float) var start_point
 export(Array, float) var end_point
 
@@ -27,49 +25,44 @@ func _process(delta):
 	var target_pos = target.global_transform.origin + Vector3.UP * 1.5
 	var self_pos = self.global_transform.origin
 	
-	distance = self_pos.distance_to(target_pos)
-	
-	var start_pos = Vector3(start_point[0], start_point[1] + 1, start_point[2])
-	var end_pos = Vector3(end_point[0], end_point[1] + 1, end_point[2])
+	var start_pos = Vector3(start_point[0], start_point[1], start_point[2])
+	var end_pos = Vector3(end_point[0], end_point[1], end_point[2])
 	
 	var self_to_start = self_pos.distance_to(start_pos)
 	var self_to_end = self_pos.distance_to(end_pos)
 	
 	if curr_state == STATES.WALK:
-		calculate_distance(self_pos, target_pos, velocity, delta)
-		state_control()
-	elif distance <= 15 and target.move_speed != 5:
-		curr_state = STATES.WALK
-
+		calculate_distance(self_pos, target_pos, velocity)
+		state_control(target_pos, self_pos)
+		
 	elif curr_state == STATES.STOP:
 		if self_to_start > 1:
-			calculate_distance(self_pos, start_pos, velocity, delta)
+			calculate_distance(self_pos, start_pos, velocity)
 		else:
 			curr_state = STATES.PATROL_FWD
 
 	elif curr_state == STATES.PATROL_FWD:
 		if self_to_end > 0.5 :
-			calculate_distance(self_pos, end_pos, velocity, delta)
+			calculate_distance(self_pos, end_pos, velocity)
 		else:
 			curr_state = STATES.PATROL_BWD
 	elif curr_state == STATES.PATROL_BWD:
 		if self_to_start > 0.5:
-			calculate_distance(self_pos, start_pos, velocity, delta)
+			calculate_distance(self_pos, start_pos, velocity)
 		else:
 			curr_state = STATES.PATROL_FWD
 
-func calculate_distance(begin: Vector3, finish: Vector3, velocity, delta):
+func calculate_distance(begin: Vector3, finish: Vector3, velocity):
 	velocity = begin.direction_to(finish).normalized()
-	if curr_state == STATES.WALK:
-		velocity.y -= GRAVITY * delta
 	move_and_slide(velocity * speed, Vector3.UP)
 	look_at(finish, Vector3.UP)
 
 
-func state_control():
+func state_control(target_pos, self_pos):
+	var distance = self_pos.distance_to(target_pos)
+
 	if distance > 15:
 		curr_state = STATES.STOP
-
 
 func distance(self_pos, target_pos):
 	return self_pos.distance_to(target_pos)
