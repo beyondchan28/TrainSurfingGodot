4f10c5cd3ea3087b8e6b1b6e555152168f161346
Chandra Gunawan <beyonddbirthday28@gmail.com>
2022-08-11 21:47:54 +0700

    Secret Building gameplay almost done

--- PathFollow.gd
+++ PathFollow.gd
@@ -1,7 +1,14 @@
 extends PathFollow
 
 
-var speed = 2
+export var speed = 2
+onready var reset_pos = get_parent().get_parent().get_node("ResetPosition").get_global_transform().origin
 
 func _process(delta):
 	set_offset(get_offset() + speed * delta)
+
+func _on_PlayerDetector_body_entered(body):
+	if body.name == "Player":
+		body.global_transform.origin = reset_pos
+	if body.name == "Ally":
+		body.global_transform.origin = reset_pos
