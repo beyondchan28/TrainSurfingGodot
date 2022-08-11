a06380dfafc44077af8082aa7a630551be8eb870
Chandra Gunawan <beyonddbirthday28@gmail.com>
2022-08-12 02:02:30 +0700

    Secret Buildings almost done

--- PathFollow.gd
+++ PathFollow.gd
@@ -2,13 +2,19 @@ extends PathFollow
 
 
 export var speed = 2
-onready var reset_pos = get_parent().get_parent().get_node("ResetPosition").get_global_transform().origin
+onready var reset_pos = get_parent().get_parent().get_node("ResetPosition")
 
 func _process(delta):
 	set_offset(get_offset() + speed * delta)
 
 func _on_PlayerDetector_body_entered(body):
 	if body.name == "Player":
-		body.global_transform.origin = reset_pos
+		body.global_transform.origin = reset_pos.get_global_transform().origin
 	if body.name == "Ally":
-		body.global_transform.origin = reset_pos
+		body.current_state = body.STATES.LOST
+
+func _on_RobotArea_body_entered(body):
+	if body.name == "Player":
+		body.global_transform.origin = reset_pos.get_global_transform().origin
+	if body.name == "Ally":
+		body.current_state = body.STATES.LOST
