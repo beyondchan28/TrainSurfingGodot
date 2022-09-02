3032d6efdfda2110e64e2ef3cd9c45c3a1b730fe
Chandra Gunawan <beyonddbirthday28@gmail.com>
2022-09-02 20:54:03 +0700

    Adding ending video and tuning sound

--- Level/SecretBuilding.gd
+++ Level/SecretBuilding.gd
@@ -1,9 +1,9 @@
 extends Spatial
 
 onready var clock = $Player/CanvasLayer/GamePlayUI/ClockBackground/Clock
+var ending_vid = preload("res://UI/Ending.ogv")
+onready var gameplay_ui = $Player/CanvasLayer
 
-#var video reward
-#onready var gameplay_ui = $Player/CanvasLayer
 func _ready():
 	clock.current_time = 5
 	$Player/Pivot/Armature/Skeleton/BoneAttachment.set_visible(true)
@@ -26,5 +26,4 @@ func _on_OffArea_area_entered(area):
 func _on_RewardArea_body_entered(body):
 	var entered = $RewardArea.get_overlapping_bodies()
 	if entered.size() == 2:
-#		gameplay_ui.play_cutscene(video reward)
-		print("Reward Video Play")
+		gameplay_ui.play_cutscene(ending_vid)
